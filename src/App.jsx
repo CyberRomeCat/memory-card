39c56c2b7b6054596ae5e7948257644f0465e1e9
jenzie68 <jacecabalo@gmail.com>
2024-10-20 21:35:24 +0400

    feat: clean code && seperate logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { FetchTitans } from './components/FetchAPI';
+import Multimedia from './components/multimedia';
 import './App.css';
+import { StrictMode } from 'react';
 
 const App = () => {
   return (
-    <>
+    <StrictMode>
       <FetchTitans />
-    </>
+      <Multimedia />
+    </StrictMode>
   );
 };
 
